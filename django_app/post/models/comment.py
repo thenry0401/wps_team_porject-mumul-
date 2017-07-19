cf666637f6cd4c2fd805228c3851e2a6f515f858
thenry0401 <thenry0401@gmail.com>
2017-07-19 23:12:25 +0900

    post모델, comment모델 추가, post뷰, comment뷰 와꾸 작성

---
+++ django_app/post/models/comment.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from config import settings
+from post.models import Post
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(settings.base.AUTH_USER_MODEL)
+    content = models.CharField(max_length=30, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
---
+++ django_app/post/views/post.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def category_post_list(request):
+    pass
+
+
+def hashtag_post_list(request):
+    pass
+
+
+def location_post_list(request):
+    pass
+
+
+def post_create(request):
+    pass
+
+
+def post_detail(request):
+    pass
+
+
+def post_modify(request):
+    pass
+
+
+def post_delete(request):
+    pass
+
+
+def post_like_toggle(request):
+    pass
+
+
+
+
+
