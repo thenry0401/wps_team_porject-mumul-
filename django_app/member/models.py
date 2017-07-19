3b4599e5ec5aafaf882d19d310c70eb1a8258118
lcgkiller <lcgkiller@gmail.com>
2017-07-19 11:21:03 +0900

    root URL 접속시 메인 화면 작성

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 from utils.fields.custom_imagefield import CustomImageField
+
+class UserManager(BaseUserManager):
+    pass
 
 
 class MyUser(AbstractUser):
