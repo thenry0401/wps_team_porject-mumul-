46c206fdd931c265474c797db5d36bce31fd6df6
thenry0401 <thenry0401@gmail.com>
2017-07-27 13:53:36 +0900

    post_create뷰 생성, PostForm생성, post/create url추가, post.html생성

--- django_app/post/views/post.py
+++ django_app/post/views/post.py
@@ -1,6 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from post.forms.post import PostForm
+from ..models import Post
+
+User = get_user_model()
+
+__all__ = (
+    'category_post_list',
+    'hashtag_post_list',
+    'location_post_list',
+    'post_create',
+    'post_delete',
+    'post_detail',
+    'post_like_toggle',
+    'post_modify',
+)
 
 
 def category_post_list(request):
@@ -16,7 +31,12 @@ def location_post_list(request):
 
 
 def post_create(request):
-    pass
+
+    form = PostForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'post_create.html', context)
 
 
 def post_detail(request):
