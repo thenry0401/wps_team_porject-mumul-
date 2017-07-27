bb22abef0c8902f7788047ffa2c06d041c4fa36e
thenry0401 <thenry0401@gmail.com>
2017-07-27 17:28:24 +0900

    post/urls.py에 app_name 추가

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from post import views
 
+app_name = 'post'
 urlpatterns = [
     url(r'^list/$', views.category_post_list, name='post_list'),
     url(r'^create/$', views.post_create, name='post_create'),
