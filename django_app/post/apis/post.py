2b051dbf18d28a8cb3b6b5277b571b93330f6727
thenry0401 <thenry0401@gmail.com>
2017-08-03 15:44:47 +0900

    PostListCreateView api 작성

--- django_app/post/apis/post.py
+++ django_app/post/apis/post.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -5,12 +6,19 @@ from ..serializers import PostSerializer
 from ..models import Post
 
 __all__ = (
-    'PostListView',
+    'PostListCreateView',
 )
 
 
-class PostListView(APIView):
+class PostListCreateView(APIView):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
