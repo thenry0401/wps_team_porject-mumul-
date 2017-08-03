4c10388c61a6da404cef20523d913d0cc7df22ea
lcgkiller <lcgkiller@gmail.com>
2017-08-03 21:15:08 +0900

    base.html에서 redirect_uri 인코딩 하기 위한 context_processors 작성

--- django_app/utils/context_processors/naver.py
+++ django_app/utils/context_processors/naver.py
@@ -1,5 +1,7 @@
 import json
 
+from django.template.defaultfilters import urlencode
+
 from config.settings.base import CONFIG_SECRET_DEPLOY_FILE
 
 __all__ = (
@@ -8,8 +10,12 @@ __all__ = (
 
 def naver_login_api_info(request):
     config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
+
+    urlencode_redirect_uri = urlencode("http://localhost:8000/member/naver-login")
+
     context = {
-        'naver_app_id': config_secret_deploy['naver']['SOCIAL_AUTH_NAVER_KEY']
+        'naver_app_id': config_secret_deploy['naver']['SOCIAL_AUTH_NAVER_KEY'],
+        'urlencode_redirect_uri': urlencode_redirect_uri
     }
 
     return context
