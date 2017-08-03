8cf7b688921caedde5dd44482972864245ce5e9a
thenry0401 <thenry0401@gmail.com>
2017-08-03 15:31:05 +0900

    PostSerializer에 필드 더 추가함

--- django_app/post/serializers/post.py
+++ django_app/post/serializers/post.py
@@ -12,5 +12,18 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = (
             'pk',
+            'author',
+            'title',
             'photo',
+            'content',
+            'created_date',
+            'modified_date',
+            'post_code',
+            'road_address',
+            'detail_address',
+            'like_users',
+            'is_sold',
+            'exchange_count',
+            'category',
+            'trading_type',
         )
