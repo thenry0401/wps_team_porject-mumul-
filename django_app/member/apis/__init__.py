7c4d45383b1431155c927e390bad3ac405704e3c
lcgkiller <lcgkiller@gmail.com>
2017-08-08 15:36:23 +0900

    Facebook 시리얼라이저 작성으로 더이상 view와 model을 사용하지 않기로 함

--- django_app/member/apis/__init__.py
+++ django_app/member/apis/__init__.py
@@ -1,1 +1,2 @@
 from .user_api import *
+from .fb_user_login import *
--- django_app/member/views/auth.py
+++ django_app/member/views/auth.py
@@ -19,7 +19,6 @@ __all__ = (
     'login',
     'logout',
     'naver_login',
-    'SocialAccountAdapter',
 )
 
 User = get_user_model()
@@ -49,34 +48,6 @@ def login(request):
 def logout(request):
     django_logout(request)
     return redirect('index')
-
-
-class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def save_user(self, request, sociallogin, form=None):
-        """
-        allauth를 통해서 유저를 저장할 때 호출됩니다. 유저 객체에 추가적인 정보를 담기 위해서 오버라이드를 실시했습니다.
-        """
-
-        user = super(SocialAccountAdapter, self).save_user(request, sociallogin, form)
-
-        # (하드코딩) social_app_name = str(request.path).split("/")[2].upper()
-        social_app_name = sociallogin.account.provider.upper()
-        extra_data = sociallogin.account.extra_data
-
-        if social_app_name == "FACEBOOK":
-            profile_url = "https://graph.facebook.com/v2.4/{user_id}/picture" \
-                          "?type=square&height={height}&width={width}" \
-                          "&return_ssl_rources=1".format(
-                            user_id=extra_data['id'],
-                            height=150,
-                            width=150,
-                            )
-            User.objects.get_or_create_facebook_user(user_pk=user.pk, extra_data=extra_data, profile_url=profile_url)
-
-        elif social_app_name == "NAVER":
-            User.objects.get_or_create_naver_user2(user_pk=user.pk, extra_data=extra_data)
-
-        return user
 
 
 def naver_login(request):
@@ -139,6 +110,7 @@ def naver_login(request):
         if 'access_token' in result:
             # result값에서 액세스 토큰을 뽑아냅니다. 이를 가지고 유저 정보를 획득할 수 있습니다.
             access_token = result.get('access_token')
+            print("금나와라 뚝딱 :", access_token)
             return access_token
         elif 'error' in result:
             raise GetAccessTokenException(result)
@@ -166,7 +138,10 @@ def naver_login(request):
 
         # Custom User Manager 내에 있는 메서드 get_or_create_naver_user를 활용해 유저를 가져옵니다.
         user = User.objects.get_or_create_naver_user(extra_data=extra_data)
-        django_login(request, user)
+        django_login(request, user, backend=AUTHENTICATION_BACKENDS[0])
+        # for k, v in request.META.items():
+        #     print(k, v)
+        # return redirect(request.META['HTTP_REFERER'])
         return redirect('index')
 
     except GetAccessTokenException as e:
