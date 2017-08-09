db2dcc3e34053fbda0a6af75953d941db8eb25d5
thenry0401 <thenry0401@gmail.com>
2017-08-09 16:49:33 +0900

    post serializer field순서 수정

--- django_app/post/serializers/post.py
+++ django_app/post/serializers/post.py
@@ -11,22 +11,24 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
+
         fields = (
             'pk',
             'author',
             'title',
             'photo',
             'content',
-            'created_date',
-            'modified_date',
+            'category',
+            'trading_type',
             'post_code',
             'road_address',
             'detail_address',
+
             'like_users',
             'is_sold',
             'exchange_count',
-            'category',
-            'trading_type',
+            'created_date',
+            'modified_date',
         )
         read_only_fields = (
             'author',
