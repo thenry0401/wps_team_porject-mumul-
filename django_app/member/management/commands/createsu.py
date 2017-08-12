3cbde1da8ed248f7107319fb4c444a942b3d6c0b
lcgkiller <lcgkiller@gmail.com>
2017-08-12 15:07:33 +0900

    default createuser를 생성(.config_secret/settings_common.json에 설정해놓은 유저를 사용하도록)

--- django_app/member/management/commands/createsu.py
+++ django_app/member/management/commands/createsu.py
@@ -1,3 +1,6 @@
+import json
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 
@@ -7,14 +10,18 @@ User = get_user_model()
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        email = options[self.User.USERNAME_FIELD]
-        password = None
+        # email = options[self.User.USERNAME_FIELD]
+        # password = None
+        config_secret_common = json.loads(open(settings.SECRET_COMMON_JSON_FILE).read())
+        username = config_secret_common['django']['default_superuser']['username']
+        password = config_secret_common['django']['default_superuser']['password']
+
         # 슈퍼유저를 만드려고할때, 해당 username을 갖는 User가 없어야 한다.
-        if not User.objects.filter(email=email).exists():
+        if not User.objects.filter(email=username).exists():
             User.objects.create_superuser(
-                email=email,
+                email=username,
                 password=password,
             )
-            print('Superuser %s is created' % email)
+            print('Superuser %s is created' % username)
         else:
-            print("Superuser %s is already exist" % email)
+            print("Superuser %s is already exist" % username)
