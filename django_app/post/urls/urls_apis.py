e7a197ad19c1d85dcd4d18ee3d75c4c786ebb31f
thenry <thenry0401@gmail.com>
2018-08-29 03:08:07 +0900

    api view 수정에 따라 url 수정

--- django_app/post/urls/urls_apis.py
+++ django_app/post/urls/urls_apis.py
@@ -7,14 +7,13 @@ urlpatterns = [
     url(r'^tag/(?P<tag_name>\w+)/$', apis.HashtagPostListView.as_view(), name='hashtag_post_list'),
     url(r'^create/$', apis.PostCreateView.as_view(), name='post_create'),
     url(r'^(?P<post_pk>\d+)/$', apis.PostDetailView.as_view(), name='post_detail'),
-    url(r'^(?P<post_pk>\d+)/modify/$', apis.PostDetailView.as_view(), name='post_modify'),
-    url(r'^(?P<post_pk>\d+)/delete/$', apis.PostDetailView.as_view(), name='post_delete'),
+    url(r'^(?P<post_pk>\d+)/modify/$', apis.PostModifyDeleteView.as_view(), name='post_modify'),
+    url(r'^(?P<post_pk>\d+)/delete/$', apis.PostModifyDeleteView.as_view(), name='post_delete'),
 
 
-    url(r'^(?P<post_pk>\d+)/comment/create/$', apis.CommentListCreateView.as_view(), name='comment_create'),
-    url(r'^(?P<post_pk>\d+)/comment/(?P<comment_pk>\d+)/$', apis.CommentDetailView.as_view(), name='comment_detail'),
-    url(r'^(?P<post_pk>\d+)/comment/(?P<comment_pk>\d+)/modify/$', apis.CommentDetailView.as_view(), name='comment_modify'),
-    url(r'^(?P<post_pk>\d+)/comment/(?P<comment_pk>\d+)/delete/$', apis.CommentDetailView.as_view(), name='comment_delete'),
+    url(r'^(?P<post_pk>\d+)/comment/create/$', apis.CommentCreateView.as_view(), name='comment_create'),
+    url(r'^(?P<post_pk>\d+)/comment/(?P<comment_pk>\d+)/modify/$', apis.CommentModifyDeleteView.as_view(), name='comment_modify'),
+    url(r'^(?P<post_pk>\d+)/comment/(?P<comment_pk>\d+)/delete/$', apis.CommentModifyDeleteView.as_view(), name='comment_delete'),
 
     # ##### 위시리스트 추가/삭제 #####
     url(r'^(?P<post_pk>[0-9]+)/wish-list/toggle/$', apis.PostLikeToggleView.as_view(), name='wishlist-toggle'),
