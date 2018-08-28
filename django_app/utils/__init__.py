08b07b44525aa3d0425e242b9758c42b445bc5bf
thenry <thenry0401@gmail.com>
2018-08-29 02:04:45 +0900

    permission 세부 수정

--- django_app/utils/__init__.py
+++ django_app/utils/__init__.py
@@ -5,4 +5,4 @@ class ObjectIsRequestUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj == request.user
+        return obj.author_id == request.user.id
